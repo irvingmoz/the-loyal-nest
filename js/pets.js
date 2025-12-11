2bcc8b3754b1a7105b16e12a95cc5769e8b4a138
irvingmoz <irving.moz328@gmail.com>
2025-12-11 01:03:26 -0600

    Create pets.js

---
+++ js/pets.js
@@ -0,0 +1,83 @@
+// Base de datos de mascotas
+const pets = [
+  {
+    id: 1,
+    nombre: "Max",
+    especie: "Perro",
+    edadCategoria: "Cachorro",
+    tamano: "Mediano",
+    imagen: "img/dog1.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Luna",
+    especie: "Gato",
+    edadCategoria: "Adulto",
+    tamano: "Chico",
+    imagen: "img/cat1.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Rocky",
+    especie: "Perro",
+    edadCategoria: "Adulto",
+    tamano: "Grande",
+    imagen: "img/dog2.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Mishi",
+    especie: "Gato",
+    edadCategoria: "Cachorro",
+    tamano: "Chico",
+    imagen: "img/cat2.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Toby",
+    especie: "Perro",
+    edadCategoria: "Senior",
+    tamano: "Mediano",
+    imagen: "img/dog3.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Nina",
+    especie: "Gato",
+    edadCategoria: "Senior",
+    tamano: "Mediano",
+    imagen: "img/cat3.jpg"
+  },
+  {
+    id: 7,
+    nombre: "Bruno",
+    especie: "Perro",
+    edadCategoria: "Adulto",
+    tamano: "Grande",
+    imagen: "img/dog4.jpg"
+  },
+  {
+    id: 8,
+    nombre: "Copito",
+    especie: "Gato",
+    edadCategoria: "Cachorro",
+    tamano: "Chico",
+    imagen: "img/cat4.jpg"
+  },
+  {
+    id: 9,
+    nombre: "Kira",
+    especie: "Perro",
+    edadCategoria: "Cachorro",
+    tamano: "Chico",
+    imagen: "img/dog5.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Salem",
+    especie: "Gato",
+    edadCategoria: "Adulto",
+    tamano: "Grande",
+    imagen: "img/cat5.jpg"
+  }
+];
